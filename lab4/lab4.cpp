967f74c5b2b22802ffbfcc120195c522d94365c2
Ручкин Игорь Сергеевич <garik@mts.ru>
2020-09-29 19:05:09 +0300

    Some mods

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,55 +1,53 @@
-﻿#include <iostream>
-//#define quantity2 = 6
+﻿#include <stdio.h>
+
 void inputArray(float*, int);
-int findSumm(float*, int, int, float);
+int findSumm(float*, int, int, int);
 int findMinimIndex(float*, float, int);
 int findMaximIndex(float*, float, int);
+int findIndex(float*, int, int);
 
-void main()
-{
-	int quantity2 = 6;
-	float line[6];
-	inputArray(line, quantity2);
-	float summa = findSumm(line, findMaximIndex(line, line[0], quantity2), findMinimIndex(line, line[0], quantity2), quantity2);
+int main() {
+	const int n = 6;
+	float line[n];
+	inputArray(line, n);
+  printf("maxIndex = %d\n", findMaximIndex(line, line[0], n));
+  printf("minIndex = %d\n", findMinimIndex(line, line[0], n));
+  float summa = findSumm(line, findIndex(line, n, 1), findIndex(line,n, -1), n);
 	printf("Summa = %f\n", summa);
 }
 
-int  findSumm(float* line, int maximIn, int minimIn, float quantity2)
-{
+int  findSumm(float* line, int maximIn, int minimIn, int n) {
 	float sum = 0;
-	for (int i = maximIn + 1; i < quantity2; i++)
+	for (int i = maximIn + 1; i < n; i++)
 		if (line[i] > (line[maximIn] + line[minimIn]) / 2)
 			sum = sum + line[i];
 	return sum;
 }
 
-void inputArray(float* array, int quantity)
-{
+void inputArray(float* array, int quantity) {
 	printf("Enter elements:\n");
-	for (int i = 0; i < quantity; i++)
-		scanf_s("%f", &array[i]);
+	for (int i = 0; i < quantity; i++) {
+    printf("%d: ", i + 1);
+		scanf("%f", &array[i]);
+  }  
 }
 
-int findMaximIndex(float* array, float maximElement, int quantity)
-{
-	int maximIndex = 0;
-	for (int i = 1; i < quantity; i++)
-		if (array[i] >= maximElement)
-		{
-			maximIndex = i;
-			maximElement = array[i];
-		}
-	return maximIndex;
-}
-
-int findMinimIndex(float* array, float minimElement, int quantity)
-{
-	int minimIndex = 0;
-	for (int i = 1; i < quantity; i++)
-		if (array[i] <= minimElement)
-		{
-			minimIndex = i;
-			minimElement = array[i];
-		}
-	return minimIndex;
+int findIndex(float* array, int quantity, int min_max) {
+	int index = 0;
+  float Element = array[0];
+	for (int i = 1; i < quantity; i++) {
+    switch (min_max) {
+      case 1:
+        if (array[i] >= Element) {
+          index = i;
+          Element = array[i];
+        }
+      case -1:
+        if (array[i] <= Element)	{
+          index = i;
+          Element = array[i];
+        }
+    }
+  }
+	return index;
 }
