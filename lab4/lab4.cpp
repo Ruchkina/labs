93375d353824ea59c63eb1ad5402450440510673
ARuchkina <71215673+Ruchkina@users.noreply.github.com>
2020-10-01 12:46:14 +0300

    Update lab4.cpp

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,39 +1,42 @@
-﻿#include <iostream>
-//#define quantity2 = 6
+#include <iostream>
+#define ARRAY_SIZE 6
 void inputArray(float*, int);
-int findSumm(float*, int, int, float);
-int findMinimIndex(float*, float, int);
-int findMaximIndex(float*, float, int);
+int findSumm(float*, float);
+int findMinimIndex(float*, int);
+int findMaximIndex(float*, int);
 
 void main()
 {
-	int quantity2 = 6;
-	float line[6];
-	inputArray(line, quantity2);
-	float summa = findSumm(line, findMaximIndex(line, line[0], quantity2), findMinimIndex(line, line[0], quantity2), quantity2);
+	float line[ARRAY_SIZE]; 
+	int arraySize = 6;
+	inputArray(line, ARRAY_SIZE);
+	float summa = findSumm(line, arraySize);
 	printf("Summa = %f\n", summa);
 }
 
-int  findSumm(float* line, int maximIn, int minimIn, float quantity2)
+int  findSumm(float* line, float size)
 {
+	int maximIn = findMaximIndex(line, size);
+	int minimIn = findMinimIndex(line, size);
 	float sum = 0;
-	for (int i = maximIn + 1; i < quantity2; i++)
+	for (int i = maximIn + 1; i < size; i++)
 		if (line[i] > (line[maximIn] + line[minimIn]) / 2)
 			sum = sum + line[i];
 	return sum;
 }
 
-void inputArray(float* array, int quantity)
+void inputArray(float* array, int size)
 {
 	printf("Enter elements:\n");
-	for (int i = 0; i < quantity; i++)
+	for (int i = 0; i < size; i++)
 		scanf_s("%f", &array[i]);
 }
 
-int findMaximIndex(float* array, float maximElement, int quantity)
+int findMaximIndex(float* array, int size)
 {
+	float maximElement = array[0];
 	int maximIndex = 0;
-	for (int i = 1; i < quantity; i++)
+	for (int i = 1; i < size; i++)
 		if (array[i] >= maximElement)
 		{
 			maximIndex = i;
@@ -42,10 +45,11 @@ int findMaximIndex(float* array, float maximElement, int quantity)
 	return maximIndex;
 }
 
-int findMinimIndex(float* array, float minimElement, int quantity)
+int findMinimIndex(float* array, int size)
 {
+	float minimElement = array[0];
 	int minimIndex = 0;
-	for (int i = 1; i < quantity; i++)
+	for (int i = 1; i < size; i++)
 		if (array[i] <= minimElement)
 		{
 			minimIndex = i;
