d6b86121435e1726f414f7d6feb62b25d4b498a5
ARuchkina <71215673+Ruchkina@users.noreply.github.com>
2020-11-09 19:58:44 +0300

    Create mystring.h

---
+++ lab8/mystring.h
@@ -0,0 +1,16 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+#define N 30
+#define LENGHT_STR 100
+void deleteGap(char* string, char*, int t);
+char** allocateMatrix(int column);
+void inputArray(char** array, int column);
+void inputSuf(char* array);
+int stringLenght(char* str);
+void changeBiggerForSmaller(int, int, char**, int, char*, int, int i);
+void changeBiggerForSmaller(char**, int, int, int, int, char* );
+void freeMatrix(char** array, int n);
+void printArray(char** array, int n);
+int findLastWords(int lenght1, char** array, char* suffix1, int indexOfSpace, int i);
+#endif
