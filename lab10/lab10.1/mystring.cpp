fa803faed9fc545abc7caea0dbb6b280a0071dca
ARuchkina <71215673+Ruchkina@users.noreply.github.com>
2020-11-28 21:46:02 +0300

    Update mystring.cpp

--- lab10/lab10.1/mystring.cpp
+++ lab10/lab10.1/mystring.cpp
@@ -26,12 +26,57 @@ STUDENT input()
     return students;
 }
 
-STUDENT print(STUDENT students)
+void output(STUDENT* students)
 {
-    printf("Name: \n");
-    printf("%s", students.name);
-    printf("Marks: \n");
+    printf("\nName:  ");
+    printf("%s", students->name);
+    printf("Marks:  ");
     for (int a = 0; a < 5; a++)
-        printf("%d", students.marks[a]);
-    return students;
+        printf("%d; ", students->marks[a]);
+    printf("\n");
+    //printf("\n%d", students->status);
 }
+
+int checkStatus(STUDENT* student)
+{
+    int status = 5;
+    for (int i = 0; i < 5; i++)
+    {
+        if (student->marks[i] < status)
+            status = student->marks[i];
+    }
+    return status;
+}
+
+void swap(STUDENT* a, STUDENT* b)
+{
+    STUDENT temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sortByStatus(STUDENT* array, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (&array[i].status < &array[j].status)
+                swap(&array[i], &array[j]);
+        }
+    }
+}
+
+void sortByABC(STUDENT* array, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if ((strcmp(array[i].name, array[j].name) > 0) && (array[i].status == array[j].status))
+                swap(&array[i], &array[j]);
+        }
+    }
+}
+
+
