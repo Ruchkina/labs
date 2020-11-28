edc960baebd4c412d3342b3658ce690e86f4993f
ARuchkina <71215673+Ruchkina@users.noreply.github.com>
2020-11-28 21:46:30 +0300

    Update mystring.h

--- lab10/lab10.1/mystring.h
+++ lab10/lab10.1/mystring.h
@@ -6,10 +6,15 @@
 typedef struct STUDENT {
     char name[NAME_LEN];
     int marks[5];
+    int status;
 } student;
 
 STUDENT input();
 STUDENT* allocateA(int n);
-STUDENT print(STUDENT students);
+void output(STUDENT* students);
+int checkStatus(STUDENT* student);
+void swap(STUDENT* a, STUDENT* b);
+void sortByStatus(STUDENT* group, int n);
+void sortByABC(STUDENT* array, int n);
 
 #endif
